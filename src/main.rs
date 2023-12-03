37736372a2e2941cd6bbec1758e918b660132e4c
ragul28 <ragulh28@gmail.com>
2023-12-03 22:37:08 +0530

    Using unwrap as shourtcut for match

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,8 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.path);
-    let content = match result {
-        Ok(content) => { content },
-        Err(error) => { return Err(error.into()); }
-    };
-
-    for line in content.lines() {
+    let result = std::fs::read_to_string(&args.path).unwrap();
+    for line in result.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
