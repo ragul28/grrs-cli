5a947b687721a6bb22a742ea7863434b9c922c54
ragul28 <ragulh28@gmail.com>
2023-12-03 23:09:32 +0530

    Split func code as lib

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path).with_context(|| format!("could not read file `{}`", args.path.display()))?;
     
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    grrs_cli::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line).expect("Clound't write to stdout");
-        }
-    }
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
 }
