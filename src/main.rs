67c475d445c549add70982b81c2b5664ce91ada6
ragul28 <ragulh28@gmail.com>
2023-12-03 22:38:17 +0530

    Another shortcut for unwrap qus mark

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.path).unwrap();
+    let result = std::fs::read_to_string(&args.path)?;
     for line in result.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
