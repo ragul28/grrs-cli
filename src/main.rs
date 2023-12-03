f19b287a765abe7d3d2a5e79d805e7bf4a89fb67
ragul28 <ragulh28@gmail.com>
2023-12-03 23:06:19 +0530

    Add the test find match

--- src/main.rs
+++ src/main.rs
@@ -23,3 +23,10 @@ fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
         }
     }
 }
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
