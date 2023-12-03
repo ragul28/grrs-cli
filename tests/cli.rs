1b73fb29b5796ba084f6572ad6ec2c0a79f1659d
ragul28 <ragulh28@gmail.com>
2023-12-03 23:33:53 +0530

    Add cli binary test case

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,22 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
 #[test]
 fn find_a_match() {
     let mut result = Vec::new();
     grrs_cli::find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
     assert_eq!(result, b"lorem ipsum\n");
+}
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs-cli")?;
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
 }
