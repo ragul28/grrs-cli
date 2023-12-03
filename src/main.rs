609eb1262d818fdef59c4767f32f5b83f4ed9232
ragul28 <ragulh28@gmail.com>
2023-12-03 22:43:38 +0530

    Custom error using anyhow lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -8,7 +9,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.path)?;
+    let result = std::fs::read_to_string(&args.path).with_context(|| format!("could not read file `{}`", args.path.display()))?;
     for line in result.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
